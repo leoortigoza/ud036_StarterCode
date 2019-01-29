bc57b9b5661ab844ef26ea0d891229b398b15d96
GRUPO04 - Rafael <ensoft48@students.ic.unicamp.br>
2019-01-29 21:33:28 -0200

    Ajustes para atender ao PEP8 online.

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,7 +5,7 @@ import yaml
 # Obtem a lista de filmes a partir de arquivo no formato yaml
 movies_list_doc = open("./movies_list.yaml")
 
-# Carrega e efetua o parse com os dados do arquivo ja no formato da classe Movie
+# Carrega e efetua o parse com os dados do arquivo no formato da classe Movie
 parsed_movies = yaml.load(movies_list_doc)
 
 movies = []
