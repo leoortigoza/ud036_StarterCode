13b3f4045a8018108a184a55655f7cec2bfeeaca
GRUPO04 - Rafael <ensoft48@students.ic.unicamp.br>
2019-01-30 11:20:15 -0200

    Adequando arquivo media.py ao PEP8.

--- media.py
+++ media.py
@@ -1,17 +1,16 @@
 class Movie():
 
-	""" Represents a way to store informations about a Movie.
+    """ Represents a way to store informations about a Movie.
 
-	Attributes:
-		 title: Movie title
-		 storyline: A briefly description about movie storyline
-		 poster_image_url: URL for Movie poster image
-		 trailer_youtube_url: URL for Movie youtube trailer 
+    Attributes:
+        title: Movie title
+        storyline: A briefly description about movie storyline
+        poster_image_url: URL for Movie poster image
+        trailer_youtube_url: URL for Movie youtube trailer"""
 
-	"""
-
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
+    def __init__(
+      self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
